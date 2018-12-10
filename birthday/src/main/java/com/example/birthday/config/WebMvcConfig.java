06c0182dad795b30b0132bc1ff4057935ab0972d
Richard <2336400051@qq.com>
2018-12-10 20:26:56 +0800

    index.html

--- birthday/src/main/java/com/example/birthday/config/WebMvcConfig.java
+++ birthday/src/main/java/com/example/birthday/config/WebMvcConfig.java
@@ -1,6 +1,11 @@
 package com.example.birthday.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,7 +15,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @description:
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -18,4 +24,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
     }
 
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        log.info("index");
+        configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/main/index"));
+    }
 }
--- birthday/src/main/java/com/example/birthday/controller/MainController.java
+++ birthday/src/main/java/com/example/birthday/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.birthday.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2018/12/7 15:24
  * @description:
  */
-@RestController
+@Controller
 @RequestMapping("/main")
 public class MainController {
 
     @GetMapping("/test")
+    @ResponseBody
     public String test(){
         return "SUCCESS!!!";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 }
